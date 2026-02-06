a84401c245e059b1914b88e84248ec26ab30b6f1
So-rush <149287154+So-rush@users.noreply.github.com>
2026-02-06 22:39:05 +0530

    Create Two Sum solution

---
+++ Arrays/TwoSum.cpp
@@ -0,0 +1,24 @@
+// LeetCode: 1. Two Sum
+// Difficulty: Easy
+// Approach: Hash Map
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
+        for (int i = 0; i < nums.size(); i++) {
+            int rem = target - nums[i];
+            if (mp.count(rem)) {
+                return {mp[rem], i};
+            }
+            mp[nums[i]] = i;
+        }
+        return {};
+    }
+};
